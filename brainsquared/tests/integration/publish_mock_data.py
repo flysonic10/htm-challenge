0b7526a7b26b54e1be250f27d5784aa1a3577f34
William Wnekowicz <william.wnekowicz@gmail.com>
2015-11-08 19:10:20 -0800

    Add classification mock publisher

--- brainsquared/tests/integration/publish_mock_data.py
+++ brainsquared/tests/integration/publish_mock_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from brainsquared.publishers.PikaPublisher import PikaPublisher
+import time
+import random
+import json
 
 USER_ID = "brainsquared"
 MODULE_IDS = ["module0", "module1", "module2", "module3"]
@@ -19,6 +22,13 @@ for module_id in MODULE_IDS:
 
 MU_KEY = '%s:%s:mu' % (USER_ID, DEVICE)
 pub.register(MU_KEY)
-pub.publish(MU_KEY, {"timestamp": 1, "value": 1})
+# pub.publish(MU_KEY, {"timestamp": 1, "value": 1})
 
+CLASSIFICATION_KEY = '%s:%s:classification' % (USER_ID, MODULE_IDS[0])
+pub.register(CLASSIFICATION_KEY)
 
+while 1:
+    random_class = random.sample([-2,-1,0,1,2], 1)[0]
+    pub.publish(CLASSIFICATION_KEY, [{"timestamp": 1, "value": random_class}])
+    print '%s - published: %i' % (CLASSIFICATION_KEY, random_class)
+    time.sleep(0.1)
